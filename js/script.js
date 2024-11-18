2231806543d800b4cd58e2aeb75399d1328ab0a9
teri.c <teri.charles23@gmail.com>
2024-11-17 20:26:22 -0400

    Created functions to fetch and display repo data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 // Div where profile information will appear
 const profileInfo = document.querySelector(".overview");
+// Ul to display the repos list
+const repoList = document.querySelector(".repo-list");
 
 const username = "teri-c";
 
@@ -29,4 +31,27 @@ const displayUserInfo = function (userProfile) {
         <p><strong>Number of public repos:</strong> ${userProfile.public_repos}</p>
     </div>`;
     profileInfo.append(div);
+
+    getRepos();
+};
+
+// Fetch the repos
+const getRepos = async function () {
+    const res = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repos = await res.json();
+    console.log(repos);
+
+    displayRepoInfo(repos);
+};
+
+
+// Display info about the repos
+const displayRepoInfo = function (repos) {
+    for (const repo of repos) {
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(li);
+    }
+
 };
